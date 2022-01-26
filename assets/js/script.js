d24f431118b5d41d8eb111dc1c12be29a001627f
taiseen <taiseen.cse@gmail.com>
2022-01-26 23:57:10 +0600

    responsive testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,27 @@
 const menuBtn = document.getElementById('menuBtn');
 const navBar = document.querySelector('.navbar');
 
-menuBtn.addEventListener('click', () => {
+const imageShow = document.querySelector('.mainHomeImage');
+const imageSlider = document.querySelectorAll('.image-slider img');
 
+// menu btn open --- close 
+menuBtn.addEventListener('click', () => {
     //menuBtn.classList.toggle('fa-times');
     menuBtn.classList.toggle('barRotate');
     navBar.classList.toggle('userClick');
 });
 
 
+// close btn when window scroll
 window.addEventListener('scroll', () => {
-
     menuBtn.classList.remove('barRotate');
     navBar.classList.remove('userClick');
+});
+
+
+imageSlider.forEach(img => {
+    img.addEventListener('click', () => {
+
+        imageShow.src = img.getAttribute('src');
+    });
 });
